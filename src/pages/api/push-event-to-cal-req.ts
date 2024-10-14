3ab869c93090b954b45295edb1b5d4d62456b30f
Jason Son <jasonson2004@gmail.com>
2024-10-14 15:20:45 -0700

    add: interface check for api req on push-event

--- src/pages/api/push-event-to-cal-req.ts
+++ src/pages/api/push-event-to-cal-req.ts
@@ -1,12 +1,11 @@
 // pages/api/push-event-to-cal-req
-// pages/api/handle-user-request
 import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
-import { AxiosError } from "axios";
-import { getCurrentStatus, parseError } from "@/utils/back";
+import { CalendarEvent } from "@/models/types";
+import firebaseClient from "@/services/firebase";
+import { parseError } from "@/utils/back";
 
 interface InterfaceResponse {
-  conversion?: object;
+  success: boolean;
   error?: string;
 }
 
@@ -15,14 +14,29 @@ export default async function handler(
   res: NextApiResponse<InterfaceResponse>,
 ) {
   if (req.method !== "POST") {
-    res.status(405).json({ error: "Was called with the wrong method." });
+    res.status(405).json({ success: false, error: "Was called with the wrong method." });
   }
 
-  const { request } = req.body;
+  const { request, token } = req.body;
+  if (!request) {
+    return res.status(401).json({
+      success: false,
+      error: "There was no valid request to process. Aborted early.",
+    });
+  }
 
-  if (!request) return res.status(401).json({
-    error: "There was no valid request to process. Aborted early.";
-  })
+  const validRequest: boolean = request satisfies CalendarEvent;
+  if (!validRequest) {
+    return res.status(500).json({
+      success: false,
+      error: "The structure of the calendar event is invalid. Aborted.",
+    });
+  }
 
-
+  try {
+    const response = await firebaseClient.insertEvent(token, request);
+    return res.status(200).json({ success: true });
+  } catch (error: any | unknown) {
+    return res.status(600).json({ success: false, error: parseError(error) });
+  }
 }
