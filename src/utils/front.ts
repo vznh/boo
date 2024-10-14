de5e0803d7ebe2bfb09736eeeedf420f9c853abe
Jason Son <jasonson2004@gmail.com>
2024-10-14 02:09:31 -0700

    add: front-end utils file

---
+++ src/utils/front.ts
@@ -0,0 +1,9 @@
+// [START utils/front]
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+// [END utils/front]
