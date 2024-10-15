3649e4974ac3ee0583d16fac013a8f03db04d08f
Jason Son <jasonson2004@gmail.com>
2024-10-14 23:56:32 -0700

    add: queue struct to ensure steady flow

--- src/utils/back.ts
+++ src/utils/back.ts
@@ -1,5 +1,6 @@
 // [START utils/back.ts]
 import { isAxiosError } from "axios";
+import { CalendarEvent } from "@/models/types";
 
 /**
  * Function for parsing errors from any error variable, most preferably in try-catch loops.
@@ -34,5 +35,41 @@ export function parseError(error: any) {
 export function getCurrentStatus() {
   return new Date().toISOString();
 }
+
+/**
+ * A queue data structure specifically meant for processing calendar events in mindful time.
+ *
+ *
+ *
+ */
+export interface EventQueue<CalendarEvent> {
+  enqueue(item: CalendarEvent): void;
+  dequeue(): CalendarEvent | undefined;
+  size(): number;
+}
+
+class EQueue<CalendarEvent> implements EventQueue<CalendarEvent> {
+  private storage: Array<CalendarEvent> = [];
+
+  constructor(private capacity: number = 5) {}
+
+  enqueue(item: CalendarEvent): void {
+    if (this.size() === this.capacity) {
+      // Load onto notification store
+      throw Error("Queue has reached max capacity, please confirm events before adding new ones.");
+    }
+    this.storage.push(item);
+  }
+
+  dequeue(): CalendarEvent | undefined {
+    return this.storage.shift();
+  }
+
+  size(): number {
+    return this.storage.length;
+  }
+}
+
+export const EQueueInst = new EQueue();
 
 // [END utils/back.ts]
