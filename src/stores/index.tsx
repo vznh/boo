e9ad7ac7d77a5fe8e13aa66afab132d255aabce5
Jason Son <jasonson2004@gmail.com>
2024-10-14 18:10:13 -0700

    add: token store for credentials

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -2,6 +2,12 @@
 import { create } from "zustand";
 import { produce } from "immer";
 import { CalendarEvent } from "@/models/types";
+import { UserCredential } from "firebase/auth";
+
+interface TokenStore {
+  token: UserCredential;
+  setToken: (token: UserCredential) => void;
+}
 
 interface CalendarStore {
   events: Array<CalendarEvent>[]; // Replace with queue later
@@ -14,4 +20,12 @@ export const useCalendarStore = create<CalendarStore>((set) => ({
     state.events.push(event);
   })),
 }));
+
+export const useTokenStore = create<TokenStore>((set) => ({
+  token: {} as UserCredential,
+  setToken: (token) => set(produce((state) => {
+    state.token = token;
+  })),
+}));
+
 // [END stores/]
