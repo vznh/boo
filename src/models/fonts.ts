134cd601c527ca49738502b8704bc371b217e25c
Jason Son <jasonson2004@gmail.com>
2024-10-16 20:59:25 -0700

    add: claude ass font

--- src/models/fonts.ts
+++ src/models/fonts.ts
@@ -1,9 +1,14 @@
 // [START models/fonts.ts]
-import { Inter, Roboto, Noto_Sans } from "next/font/google";
+import { Inter, Quattrocento, Noto_Sans } from "next/font/google";
 
 export const InterHeading = Inter({
   weight: "200",
   subsets: ["latin"],
+});
+
+export const QuatHeading = Quattrocento({
+  weight: "400",
+  subsets: ["latin"]
 });
 
 export const InterBody = Inter({
